aa81989fb3d747266cc83bb35aa9f8ff36bbf6f9
pu.cheung@zoho.com <pu.cheung@zoho.com>
2018-12-03 09:06:32 +0800

    first commit

---
+++ genlist.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O gfw.tmp
+wget https://hg.adblockplus.org/easylistchina/raw-file/tip/easylistchina.txt -O adb.list
+
+cat gfw.tmp |base64 -d >> gfw.list
+cat gfw.list |grep -E '^\|\|[^/*]*$' | sed 's/|//g' | awk '{ print "gfw/"$1 }' >> update.tmp
+cat adb.list |grep -E '^\|\|[^/*]*\^$' | sed 's/|//g' | sed 's/\^//g' | awk '{ print "adb/"$1 }' >> update.tmp
+cat update.tmp | sort | uniq >> update.conf
+
+rm -f gfw.tmp update.tmp gfw.list adb.list
+
