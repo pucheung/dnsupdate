2fcb0413949ce2678b140bebce6e3ccaae7ebb7a
pu <pu@route.lo>
2019-02-04 03:53:42 -0500

    update 2019/02/04

--- genlist.sh
+++ genlist.sh
@@ -7,6 +7,7 @@ wget https://hg.adblockplus.org/easylistchina/raw-file/tip/easylistchina.txt -O 
 cat gfw.tmp |base64 -d >> gfw.list
 cat gfw.list |grep -E '^\|\|[^/*]*$' | sed 's/|//g' | awk '{ print "gfw/"$1 }' >> update.tmp
 cat adb.list |grep -E '^\|\|[^/*]*\^$' | sed 's/|//g' | sed 's/\^//g' | awk '{ print "adb/"$1 }' >> update.tmp
+cat my.list >>update.tmp
 cat update.tmp | awk '{ print $1 }' | sort | uniq >> update.conf
 
 rm -f gfw.tmp update.tmp gfw.list adb.list
